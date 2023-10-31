18d26efb011d0268f4fc58296dda4f0b9af7574a
Havish <Havish747@gmail.com>
2023-10-30 22:31:25 -0400

    Day 1

--- day1.go
+++ day1.go
@@ -1,47 +1,34 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"strconv"
 )
 
-func read_file(file_name string) ([]string, error) {
-	// Open the file for reading
-	file, err := os.Open(file_name) // Replace "yourfile.txt" with the path to your file
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return nil, err
-	}
-	defer file.Close() // Close the file when we're done
-
-	// Create a scanner to read the file line by line
-	scanner := bufio.NewScanner(file)
-	file_lines := make([]string, 0)
-	// Read and convert each line to an integer
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		file_lines = append(file_lines, line)
-	}
-
-	if scanner.Err() != nil {
-		fmt.Println("Error reading the file:", scanner.Err())
-	}
-	return file_lines, nil
-}
-
-func main() {
-	// num, err := strconv.Atoi(line)
-	file_lines, _ := read_file("day1.txt")
+func day1_helper(n int) int {
 	sum := 0
 
+	for {
+		n = (n / 3) - 2
+		if n <= 0 {
+			break
+		}
+		sum += n
+	}
+	return sum
+}
+
+func day1_func() {
+	// num, err := strconv.Atoi(line)
+	file_lines, _ := read_file("day1.txt")
+	sum1 := 0
+	sum2 := 0
 	for _, line := range file_lines {
 		num, _ := strconv.Atoi(line)
-
-		sum += (num / 3) - 2
+		sum1 += (num / 3) - 2
+		sum2 += day1_helper(num)
 	}
-	fmt.Println(sum)
+
+	println(sum1)
+	println(sum2)
 
 }
